25cd61cbc78e2c68573a31f0224ba92059f64ae2
Hoang T Nguyen <143175225+hoangtng@users.noreply.github.com>
2026-05-19 12:39:15 -1000

    fix: add scheme to API rewrite destination

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'standalone',
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/api"}/:path*`,
+        destination: `${process.env.API_BACKEND_URL}/api/:path*`,
       },
     ];
   },

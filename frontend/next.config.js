d72f31905dab46ae11225336ddd5c6edce0b2866
Hoang T Nguyen <143175225+hoangtng@users.noreply.github.com>
2026-05-19 13:18:24 -1000

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,15 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  // remove to call backend directly
-  /*async rewrites() {
+  async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.API_BACKEND_URL}/api/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
       },
     ];
-  },*/
+  },
 };
 
 module.exports = nextConfig;
